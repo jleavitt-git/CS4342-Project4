493205c9a841e4abd9b51b3125be1bfca939ad50
eschuman20 <93109735+eschuman20@users.noreply.github.com>
2023-04-24 15:11:34 -0400

    Update homework5_template.py

--- homework5_template.py
+++ homework5_template.py
@@ -17,10 +17,10 @@ def forward_prop (x, y, W1, b1, W2, b2):
     z = np.dot(W1, x) + b1[:, np.newaxis]
     h = relu(z)
     yhat = np.dot(W2, h) + b2
-    loss = 1/(2*y.size) * (np.sum(y-yhat)**2)
-    # print(f"------Stats------\nW1 = {W1.shape}\nW2 = {W2.shape}\nb1 = {b1.shape}\nb2 = {b2}\nx = {x.shape}\ny = {y}\nyhat = {yhat}\nz = {z.shape}\nh = {h.shape}\nloss = {loss}")
+    loss = (1/(2*y.size)) * (np.sum((y - yhat) ** 2))
+    #print(f"------Stats------\nW1 = {W1.shape}\nW2 = {W2.shape}\nb1 = {b1.shape}\nb2 = {b2}\nx = {x.shape}\ny = {y}\nyhat = {yhat}\nz = {z.shape}\nh = {h.shape}\nloss = {loss}")
     return loss, x, z, h, yhat
-   
+
 def back_prop (X, y, W1, b1, W2, b2):
     loss, x, z, h, yhat = forward_prop(X, y, W1, b1, W2, b2)
     #print (y.size)
@@ -35,19 +35,24 @@ def back_prop (X, y, W1, b1, W2, b2):
     gradb1 = np.mean(gT.T, axis = 1) #average across all training examples
     return gradW1, gradb1, gradW2, gradb2
 
-def train (trainX, trainY, W1, b1, W2, b2, testX, testY, epsilon = 1e-2, batchSize = 64, numEpochs = 1000):
-    print("Starting to train...")
+def train (trainX, trainY, W1, b1, W2, b2, testX, testY, epsilon = 1e-5, batchSize = 128, numEpochs = 250):
+    #print (trainX.shape)
+    #print (trainY.shape)
     for i in range (numEpochs):
+        if i <= (numEpochs*.1):
+            epsilon = 1e-4
+        elif i <= (numEpochs*.5):
+            epsilon = 1e-5
+        else: epsilon = 1e-6
         if i % 500 == 0 and i != 0:
             print(f"Progress {i/50}%")
-        for i in range((int(trainY.size/batchSize)) - 1):
-            gradW1, gradb1, gradW2, gradb2 = back_prop(trainX[:, i*batchSize:(i*batchSize) + batchSize], trainY[i*batchSize:(i*batchSize) + batchSize], W1, b1, W2, b2)
+        for j in range((int(trainY.size/batchSize)) - 1):
+            gradW1, gradb1, gradW2, gradb2 = back_prop(trainX[:, j*batchSize:(j*batchSize) + batchSize], trainY[j*batchSize:(j*batchSize) + batchSize], W1, b1, W2, b2)
             W1 = W1-(epsilon*gradW1)
             W2 = W2-(epsilon*gradW2)
             b1 = b1-(epsilon*gradb1)
             b2 = b2-(epsilon*gradb2)
-        # print(f"------Stats------\nW1 = {W1}\nW2 = {W2}\nb1 = {b1}\nb2 = {b2}")
-    #Was b2 supposed to be b2W
+        #print(f"------Stats------\nW1 = {W1}\nW2 = {W2}\nb1 = {b1}\nb2 = {b2}")
     return W1, b1, W2, b2
 
 def show_weight_vectors (W1):
@@ -95,11 +100,27 @@ if __name__ == "__main__":
     # Note: the gradients shown below assume 20 hidden units.
     checkGradient()
 
-    # Initialize weights to reasonable random values
-    W1 = 2*(np.random.random(size=(NUM_HIDDEN, NUM_INPUT))/NUM_INPUT**0.5) - 1./NUM_INPUT**0.5
-    b1 = 0.01 * np.ones(NUM_HIDDEN)
-    W2 = 2*(np.random.random(size=(NUM_OUTPUT, NUM_HIDDEN))/NUM_HIDDEN**0.5) - 1./NUM_HIDDEN**0.5
-    b2 = np.mean(trainY)
-
-    # Train NN
-    W1, b1, W2, b2 = train(trainX, trainY, W1, b1, W2, b2, testX, testY)
+    import csv
+    with open ('HyperParamAccuracies.csv', mode='w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(['hiddenLayers', 'epochs', 'batchSize', 'LossTrain', 'LossTest'])
+        hiddenLayers = [5, 10, 20, 30]
+        epochs = [50, 100, 250, 500]
+        batchSizes = [32, 64, 128, 256]
+        for hl in hiddenLayers:
+            for ep in epochs:
+                for bs in batchSizes:
+                    # Initialize weights to reasonable random values
+                    W1 = 2*(np.random.random(size=(hl, NUM_INPUT))/NUM_INPUT**0.5) - 1./NUM_INPUT**0.5
+                    b1 = 0.01 * np.ones(hl)
+                    W2 = 2*(np.random.random(size=(NUM_OUTPUT, hl))/hl**0.5) - 1./hl**0.5
+                    b2 = np.mean(trainY)
+                    # Train NN
+                    W1, b1, W2, b2 = train(trainX, trainY, W1, b1, W2, b2, testX, testY, batchSize = bs, numEpochs=ep)
+                    #Evaluate Accuracy
+                    loss, x, z, h, yhat = forward_prop(trainX, trainY, W1, b1, W2, b2)
+                    lossTest, xTest, zTest, hTest, yhatTest = forward_prop(testX, testY, W1, b1, W2, b2)
+                    print (f"Train Accuracy: hiddenLayers = {hl}, epochs = {ep}, batchSize = {bs}, Loss = {loss}")
+                    print (f"Test Accuracy: hiddenLayers = {hl}, epochs = {ep}, batchSize = {bs}, Loss = {lossTest}")
+                    writer.writerow([hl, ep, bs, loss, lossTest])
+                    #show_weight_vectors(W1)
